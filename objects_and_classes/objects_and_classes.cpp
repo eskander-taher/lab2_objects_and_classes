848348f6c97a228753ebf51b1b0a97b9779dc63b
eskander-taher <eskander.alshaibany@gmail.com>
2021-12-04 02:56:28 +0500

    feature: add draw function

--- objects_and_classes/objects_and_classes.cpp
+++ objects_and_classes/objects_and_classes.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 
 using namespace std;
-//function for styling
+//functions for styling
 void  highlight (string s);
 void  put_line  (string s);
 void  yellow    (string s);
@@ -23,11 +23,11 @@ public:
         width  = 0;
     }
 
-    Reqtangle(int x, int y)
+    Reqtangle(int req_length, int req_width)
     {
         grean("Reqtangle(int x, int y)");
-        this->length = x;
-        this->width  = y;
+        this->length = req_length;
+        this->width  = req_width;
     }
 
     Reqtangle(Reqtangle &obj)
@@ -42,13 +42,33 @@ public:
         printf("\n~Reqtangle() ");
         printf(" length = %d, width = %d\n", length, width);
     }
+    void draw();
 };
 
+void Reqtangle::draw()
+{
+    int a, b;
+
+    grean("draw");
+    printf("\033[1;32m");
+    for (a = 0; a != length; a++) 
+    {
+        for (b = 0; b != width; b++) 
+        {
+            if ((a == 0) || (a == length - 1) || (b == width - 1) || (b == 0))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\033[0m");
+}
 
 int main()
 {
     yellow("====================================");
-    highlight("Static allocation");
+    highlight("Static allocation...................");
     {
         Reqtangle req1;
         Reqtangle req2(2, 3); 
@@ -57,10 +77,11 @@ int main()
     yellow("====================================");
 
     yellow("====================================");
-    highlight("Dynamic allocation");
+    highlight("Dynamic allocation..................");
     Reqtangle *req1 = new Reqtangle();
-    Reqtangle *req2 = new Reqtangle(4,5);
+    Reqtangle *req2 = new Reqtangle(5,10);
     Reqtangle *req3 = new Reqtangle(*req2);
+    req2->draw();
     delete     req1;
     delete     req2;
     delete     req3;
